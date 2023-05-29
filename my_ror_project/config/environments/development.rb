a430e43514913ff5a9d96d7f7686443b752a5dc6
NAEEM-BYTRADE\naeem <naeem@bytrade.io>
2023-05-29 12:09:09 +0400

    Allow Permission

--- my_ror_project/config/environments/development.rb
+++ my_ror_project/config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Allow Permissions
+  config.hosts << "myrorapplication-naeem99k.b4a.run"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -68,5 +71,3 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
-
-config.hosts << "myrorapplication-naeem99k.b4a.run"
